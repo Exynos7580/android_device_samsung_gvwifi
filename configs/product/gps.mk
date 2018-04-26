39718ebe8c8812555f0a64f7e1ea4bf7d5525337
deadman96385 <seanhoyt963@gmail.com>
2018-04-26 14:11:52 -0500

    Build required gps packages

--- configs/product/gps.mk
+++ configs/product/gps.mk
@@ -23,6 +23,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/gps.conf:system/etc/gps.conf \
     $(LOCAL_PATH)/configs/gps/gps.xml:system/etc/gps.xml
 
+
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl \
-    android.hardware.gnss@1.0-service
+    libprotobuf-cpp-fl26 \
+    libprotobuf-cpp-full \
+    libsecril-client \
+    libxml2
