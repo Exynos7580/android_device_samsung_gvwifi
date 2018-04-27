214c596b24565954053af0438121deae29526d03
deadman96385 <seanhoyt963@gmail.com>
2018-04-27 17:05:26 +0000

    Build aosp gps
    Don't shim sensor service
    
    Change-Id: I445cef57943092a6321348bf9525fb91fe1e9201

--- configs/board/shims.mk
+++ configs/board/shims.mk
@@ -22,5 +22,5 @@
 TARGET_LD_SHIM_LIBS += \
     /system/bin/gpsd|/vendor/lib/libshim_gpsd.so \
     /system/lib/hw/camera.universal7580.so|/vendor/lib/libshim_camera.so \
-    /system/lib/libexynoscamera.so|/vendor/lib/libexynoscamera_shim.so \
-    /system/lib/libsensorservice.so|/vendor/lib/libshim_sensor.so
+    /system/lib/libexynoscamera.so|/vendor/lib/libexynoscamera_shim.so
+#    /system/lib/libsensorservice.so|/vendor/lib/libshim_sensor.so
--- configs/product/gps.mk
+++ configs/product/gps.mk
@@ -23,6 +23,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/gps.conf:system/etc/gps.conf \
     $(LOCAL_PATH)/configs/gps/gps.xml:system/etc/gps.xml
 
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-service
 
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-fl26 \
