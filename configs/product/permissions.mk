5d3c2895bcbe786f990cec9cbe1488b86d1df823
deadman96385 <seanhoyt963@gmail.com>
2018-04-27 16:50:58 -0500

    Add custom handheld core permission
    Disabled general camera, compaass, accelerometer

--- configs/product/permissions.mk
+++ configs/product/permissions.mk
@@ -20,4 +20,5 @@
 #####################
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    $(LOCAL_PATH)/configs/permissions/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
