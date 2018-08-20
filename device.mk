6d25b428134fe11a805045e101e0c1983951e4c3
Sean Hoyt <seanhoyt963@gmail.com>
2018-08-19 22:52:29 -0500

    Correct audio/bt includes

--- device.mk
+++ device.mk
@@ -8,8 +8,8 @@ DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths_0.xml
+    $(DEVICE_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths_0.xml
 
 # Bluetooth
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
+    $(DEVICE_PATH)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
